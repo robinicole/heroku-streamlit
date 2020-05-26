efe33311f93866ab494f59388b69b4ab6f13fbbc
Robin Nicole <robinnicole@iphonedumbreras.home>
2020-05-26 22:19:45 +0100

    remove homeview

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import seaborn as sns
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-class HomepagView:
-    name = 'homepage'
-    def display(self):
-         st.title('Homepage')
-         return 
+from views.home import HomepageView
 
-views_list = [HomepagView]
-views_dict = {view.name: view() for view in views_list}
+views_list = [HomepageView]
+views_dict = {
+    view.name: view()
+     for view in views_list
+     }
+
 def main():
     page = st.sidebar.selectbox("Choose a page", list(views_dict.keys()))
     views_dict[page].display()
